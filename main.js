f099105974ba04ce05ddac900dfb626c9c2616dc
Jaydson Gomes <jayalemao@gmai.com>
2013-07-19 13:11:22 -0300

    Added layer with text. Improved bubble animations

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
 		this.bubblesSizes = [80,120,180,200,250];
 		this.zIndex = 0;
 
+		this.getRandom = function (from, to) {
+		    return Math.floor(Math.random() * (to - from + 1) + from);
+		};
+
 		this.toggleAnimation = function (toggle) {
 			var elements = document.querySelectorAll('.bubble'),
 				i = 0,
@@ -18,15 +22,26 @@
 			}
 		};
 
+		this.showLayer = function () {
+			var width = document.body.offsetWidth + 'px',
+				height = document.body.offsetHeight + 'px',
+				elem = document.querySelector('#layer');
+			elem.style.width = width;
+			elem.style.height = height;
+			elem.style.display = 'block';
+			elem.style.zIndex = this.zIndex + 1;
+		};
+
 		this.createBubble = function () {
 			var that = this,
 				element = document.createElement('div'),
 				mainBubble = document.querySelector('#bubbles'),
-				size = this.bubblesSizes[Math.ceil(Math.random() * 3)],
+				size = that.getRandom(50,280),
 				animation = (Math.ceil(Math.random() * 2) === 1) ? 'moveUp' : 'moveDown',
-				moveDownDuration = Math.ceil(Math.random() * 80) + 's',
-				sideDuration = Math.ceil(Math.random() * 5) + 's',
-				left = Math.ceil(Math.random() * 300) + 'px',
+				moveDownDuration = that.getRandom(10, 80) + 's',
+				sideDuration = that.getRandom(2, 5) + 's',
+				left = that.getRandom(-200, 300) + 'px',
+				top = that.getRandom(-50, 500) + 'px',
 				img = document.createElement('img');
 
 			this.zIndex ++;
@@ -37,6 +52,7 @@
 			element.style.width = size + 'px';
 			element.style.height = size + 'px';
 			element.style.left = left;
+			element.style.top = top;
 			element.zIndex = this.zIndex;
 			element.style.background = '#'+Math.floor(Math.random()*16777215).toString(16); // Credit >>> http://www.paulirish.com/2009/random-hex-color-code-snippets/
 			element.style.MozAnimation = animation + ' ' + moveDownDuration + ' linear infinite, sideWays ' + sideDuration + ' ease-in-out infinite alternate';
@@ -47,6 +63,8 @@
 			element.addEventListener('click', function () {
 				this.style.zIndex = that.zIndex + 1; //that.toggleAnimation(1);
 				that.zIndex++;
+				this.setAttribute('class','bubble paused');
+				that.showLayer();
 			});
 			mainBubble.appendChild(element);
 		};
